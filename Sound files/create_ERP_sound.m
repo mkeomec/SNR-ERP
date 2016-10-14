478cca3c74c1c27bff92142b0e8da5360b83e1c6
Michael Lee <selicas@gmail.com>
2016-10-14 16:29:06 -0700

    Completed code sections 3-6

--- Sound files/create_ERP_sound.m
+++ Sound files/create_ERP_sound.m
@@ -3,22 +3,53 @@ function create_ERP_sound(subid, audiogram)
 % This function receives audiogram information and outputs appropriate
 % sound files for the SNR-ERP study, Project AE. Sound intensity is
 % calculated based on 30 db above PTA db HL converted to db SPL
+
+% TO DO: Automatically import audiogram information from spreadsheet so
+% "audiogram" input will not be necessary
 % 
 % Inputs:
 %  1. subid = 4-digit Subject ID for SNR and SNR-ERP study. UW site
-%             subjects begin with 1
-%  2. audiogram = 2x3 array 
-%                     ex. [10,20,30;10,20,30]
+%             subjects begin with 1. MUST ENTER AS STRING
+%  2. audiogram = 2x3 array of HL at the following frequencies: 500,
+%                   1000, 2000      ex. [10,20,30;10,20,30]
+
 
 % Table of Contents:
 % 1. Import sound files
 % 2. Calculate PTA
+% 3. Add 30 db and convert to SPL
+% 4. Create sound stimuli
+% 5. Create Sound Stimuli
+% 6. Create Noise Stimuli
+
 
 % 1. Import sound files
 
-noise65db=audioread('Noise65.wav')
-stim65db=audioread('stim65.wav')
+noise65db=audioread('Noise65.wav');
+stim65db=audioread('stim65.wav');
 
 % 2. Calculate pure tone average (PTA)
-
+%  Average 
 PTA=mean(mean(audiogram,2))
+
+% 3. Add 30 db HL and convert to SPL for frequency 1000 Hz
+PTA30=PTA+30
+SPL30=PTA30+7.5
+
+% 4. Create sound stimuli 
+stim=stim65db*(db2amp(SPL30-65));
+
+% 5. Create Noise stimuli
+noiseSNR_5=noise65db*(db2amp(SPL30-70));
+noiseSNR0=noise65db*(db2amp(SPL30-65));
+noiseSNR5=noise65db*(db2amp(SPL30-60));
+noiseSNR10=noise65db*(db2amp(SPL30-55));
+noiseSNR15=noise65db*(db2amp(SPL30-50));
+
+% 6. Output sound files
+audiowrite(strcat(subid,'stim.wav'),stim65db,10000)
+audiowrite(strcat(subid,'noiseSNR_5.wav'),noiseSNR_5,44100)
+audiowrite(strcat(subid,'noiseSNR0.wav'),noiseSNR0,44100)
+audiowrite(strcat(subid,'noiseSNR5.wav'),noiseSNR5,44100)
+audiowrite(strcat(subid,'noiseSNR10.wav'),noiseSNR10,44100)
+audiowrite(strcat(subid,'noiseSNR15.wav'),noiseSNR15,44100)
