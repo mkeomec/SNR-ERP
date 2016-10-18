d6ba2efad22cbc8519f1815844603d045b74e513
Michael Lee <selicas@gmail.com>
2016-10-18 12:11:31 -0700

    Added transducer, updated section 5 and 6
    Added input for transducer, corrected values for decibel change in
    Section 5, updated section 6 for subid to be input as numerical instead
    of string

--- Sound files/create_ERP_sound.m
+++ Sound files/create_ERP_sound.m
@@ -1,4 +1,4 @@
-function create_ERP_sound(subid, audiogram)
+function create_ERP_sound(subid, transducer)
 
 % This function receives audiogram information and outputs appropriate
 % sound files for the SNR-ERP study, Project AE. Sound intensity is
@@ -10,8 +10,7 @@ function create_ERP_sound(subid, audiogram)
 % Inputs:
 %  1. subid = 4-digit Subject ID for SNR and SNR-ERP study. UW site
 %             subjects begin with 1. MUST ENTER AS STRING
-%  2. audiogram = 2x3 array of HL at the following frequencies: 500,
-%                   1000, 2000      ex. [10,20,30;10,20,30]
+%  2. transducer = Enter type of tranducer (1= TDH-50, 2= ER4)
 
 
 % Table of Contents:
@@ -25,9 +24,10 @@ function create_ERP_sound(subid, audiogram)
 % 0. Import audiogram
 
 
-audio=readtable('audiograms.csv')
-rows = audio.subject_id==subid
-
+audio=readtable('audiograms.csv');
+rows = audio.subject_id==subid;
+audiogram=audio(rows,:);
+audiogram=[audiogram.right_air_conduction_500 audiogram.right_air_conduction_1000 audiogram.right_air_conduction_2000 audiogram.left_air_conduction_500 audiogram.left_air_conduction_1000 audiogram.left_air_conduction_2000]
 % 1. Import sound files
 
 noise65db=audioread('Noise65.wav');
@@ -39,22 +39,30 @@ PTA=mean(mean(audiogram,2))
 
 % 3. Add 30 db HL and convert to SPL for frequency 1000 Hz
 PTA30=PTA+30
-SPL30=PTA30+7.5
+
+switch transducer
+    case 1
+        SPL30=PTA30+7.5
+    
+    case 2
+        SPL30=PTA30    
+end
 
 % 4. Create sound stimuli 
 stim=stim65db*(db2amp(SPL30-65));
 
 % 5. Create Noise stimuli
-noiseSNR_5=noise65db*(db2amp(SPL30-70));
+noiseSNR_5=noise65db*(db2amp(SPL30-60));
 noiseSNR0=noise65db*(db2amp(SPL30-65));
-noiseSNR5=noise65db*(db2amp(SPL30-60));
-noiseSNR10=noise65db*(db2amp(SPL30-55));
-noiseSNR15=noise65db*(db2amp(SPL30-50));
+noiseSNR5=noise65db*(db2amp(SPL30-70));
+noiseSNR10=noise65db*(db2amp(SPL30-75));
+noiseSNR15=noise65db*(db2amp(SPL30-80));
 
 % 6. Output sound files
-audiowrite(strcat(subid,'stim.wav'),stim65db,10000)
-audiowrite(strcat(subid,'noiseSNR_5.wav'),noiseSNR_5,44100)
-audiowrite(strcat(subid,'noiseSNR0.wav'),noiseSNR0,44100)
-audiowrite(strcat(subid,'noiseSNR5.wav'),noiseSNR5,44100)
-audiowrite(strcat(subid,'noiseSNR10.wav'),noiseSNR10,44100)
-audiowrite(strcat(subid,'noiseSNR15.wav'),noiseSNR15,44100)
+subjectid=num2str(subid)
+audiowrite(strcat(subjectid,'stim.wav'),stim65db,10000)
+audiowrite(strcat(subjectid,'noiseSNR_5.wav'),noiseSNR_5,44100)
+audiowrite(strcat(subjectid,'noiseSNR0.wav'),noiseSNR0,44100)
+audiowrite(strcat(subjectid,'noiseSNR5.wav'),noiseSNR5,44100)
+audiowrite(strcat(subjectid,'noiseSNR10.wav'),noiseSNR10,44100)
+audiowrite(strcat(subjectid,'noiseSNR15.wav'),noiseSNR15,44100)
