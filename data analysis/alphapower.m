72f627887d6617c1b8b820ad1e146710cf5375da
Michael Lee <selicas@gmail.com>
2016-11-08 00:14:25 -0800

    Developing alphapower  and preprocess
    Alphapower needs to pass processed data to next step. Need to merge
    trials into a single trial. Could not figure out how to pass EEGLAB data
    to fieldtrip

--- data analysis/alphapower.m
+++ data analysis/alphapower.m
@@ -2,7 +2,7 @@ function alphapower
 
 % Create layout in Fieldtrip
 
-% Visualize EEG data in data browser
+%% Visualize EEG data in data browser
 cfg = [];
 cfg.dataset = '1016_KDT_09_30_2016.cnt';
 cfg.channel = 'EEG';
@@ -17,15 +17,45 @@ set(gcf, 'Position',[1 1 1200 800])
 print -dpng natmeg_databrowser2.png
 
 
-% % Preprocessing: Reference channel set to Cz. 
+%% Preprocessing: Reference channel set to Cz. 
 % 
 % cfg = [];
 % cfg.dataset     = '1016_KDT_09_30_2016.cnt';
 % cfg.reref       = 'yes';
 
 
-% Define Trial 
+% %% Define trial
+% 
+% cfg = [];
+% cfg.dataset = '1016_KDT_09_30_2016.cnt';
+% cfg.trialfun = 'ft_trialfun_general'; % this is the default
+% cfg.trialdef.eventtype  = 'trigger'; %specify event type
+% cfg.trialdef.eventvalue  = 22; %specify trigger value;
+% cfg.trialdef.poststim=120
+% cfg.trialdef.prestim=0
+% %cfg.trl = cfg.trialdef.triallength;
+% cfg = ft_definetrial(cfg);
+% trl=cfg.trl
+% 
+% cfg.trl = trl; 			% saved somewhere previously
+% trialdata = ft_preprocessing(cfg);	% call preprocessing, putting the output in ‘trialdata’
+% 
+% 
+% %% Preprocessing: Reference channel set to Cz. 
+% cfg = [];
+% cfg.dataset = '1016_KDT_09_30_2016.cnt';
+% cfg.continuous  = 'yes';
+% cfg.channel     = 'all';
+% %cfg.trl = 'data_eeg.trl'
+% cfg.bpfilter = 'yes';
+% cfg.bpfreq = [.5 50];  
+% cfg.reref       = 'yes';
+% cfg.refmethod     = 'avg'
+% cfg.implicitref = 'Cz';            % the implicit (non-recorded) reference channel is added to the data representation
+% cfg.refchannel     = {'Cz', '65'}; % the average of these channels is used as the new reference, note that channel '53' corresponds to the right mastoid (M2)
+% data_eeg        = ft_preprocessing(cfg);
 
+%% Define Trial
 cfg = [];
 cfg.dataset = '1016_KDT_09_30_2016.cnt';
 cfg.trialfun = 'ft_trialfun_general'; % this is the default
@@ -38,12 +68,9 @@ cfg = ft_definetrial(cfg);
 trl=cfg.trl
 
 cfg.trl = trl; 			% saved somewhere previously
-cfg.dataset = '1016_KDT_09_30_2016.cnt';  	% data file, you might also need to add the path to it 
-trialdata = ft_preprocessing(cfg);	% call preprocessing, putting the output in ‘trialdata’
 
 %% Preprocessing: Reference channel set to Cz. 
-cfg = [];
-cfg.dataset = '1016_KDT_09_30_2016.cnt';
+
 cfg.continuous  = 'yes';
 cfg.channel     = 'all';
 %cfg.trl = 'data_eeg.trl'
@@ -55,26 +82,25 @@ cfg.implicitref = 'Cz';            % the implicit (non-recorded) reference chann
 cfg.refchannel     = {'Cz', '65'}; % the average of these channels is used as the new reference, note that channel '53' corresponds to the right mastoid (M2)
 data_eeg        = ft_preprocessing(cfg);
 
-
 %% ICA
 
-cfg = [];
-cfg.channel = 'EEG';
-ic_data = ft_componentanalysis (cfg,trialdata);
-cfg = [];
-cfg.viewmode = 'component';
-cfg.continuous = 'yes';
-cfg.layout = 'quickcap64.mat';
-cfg.blocksize = 1;
-cfg.channels = [1:10];
-ft_databrowser(cfg,ic_data);
+% cfg = [];
+% cfg.channel = 'EEG';
+% ic_data = ft_componentanalysis (cfg,trialdata);
+% cfg = [];
+% cfg.viewmode = 'component';
+% cfg.continuous = 'yes';
+% cfg.layout = 'quickcap64.mat';
+% cfg.blocksize = 1;
+% cfg.channels = [1:10];
+% ft_databrowser(cfg,ic_data);
 
 
 %% Artifact Detection (Auto)
 
 % muscle
 cfg = [];
-cfg.dataset     = '1016_KDT_09_30_2016.cnt';
+cfg.dataset     = data_eeg;
 cfg.continuous = 'yes';
 cfg.trl = trl;
 % channel selection, cutoff and padding
@@ -85,7 +111,7 @@ cfg.artfctdef.zvalue.trlpadding  = 0;
 cfg.artfctdef.zvalue.fltpadding  = 0;
 cfg.artfctdef.zvalue.artpadding  = 0.1;
 cfg.artfctdef.zvalue.artfctpeak  = 'no'
-cfg.artfctdef.zvalue.interactive = 'yes'
+cfg.artfctdef.zvalue.interactive = 'no'
 
 
 %parameters
@@ -95,13 +121,13 @@ cfg.artfctdef.zvalue.bpfreq = [110 140];
 cfg.artfctdef.zvalue.bpfiltord   = 9;
 cfg.artfctdef.zvalue.bpfilttype  = 'but';
 
-[cfg, artifact_muscle] = ft_artifact_zvalue(cfg);
+[cfg, artifact_muscle] = ft_artifact_zvalue(cfg,data_eeg);
 
 
 % EOG Artifact rejection
 
 cfg = [];
-cfg.dataset     = '1016_KDT_09_30_2016.cnt';
+cfg.dataset     = data_eeg;
 cfg.continuous = 'yes';
 cfg.trl = trl;
 cfg.artfctdef.zvalue.channel = 'all';
@@ -110,7 +136,7 @@ cfg.artfctdef.zvalue.trlpadding  = 0;
 cfg.artfctdef.zvalue.fltpadding  = 0;
 cfg.artfctdef.zvalue.artpadding  = 0.1;
 cfg.artfctdef.zvalue.artfctpeak  = 'no'
-cfg.artfctdef.zvalue.interactive = 'yes'
+cfg.artfctdef.zvalue.interactive = 'no'
 
 %parameters
 % the current values are based off the Fieldtrip tutorials
@@ -119,7 +145,7 @@ cfg.artfctdef.zvalue.bpfreq = [1 15];
 cfg.artfctdef.zvalue.bpfiltord   = 4;
 cfg.artfctdef.zvalue.bpfilttype  = 'but';
 
-[cfg, artifact_EOG] = ft_artifact_zvalue(cfg);
+[cfg, artifact_EOG] = ft_artifact_zvalue(cfg,data_eeg);
 
 
 % 
@@ -148,33 +174,48 @@ cfg.artfctdef.zvalue.bpfilttype  = 'but';
 %  
 %    [cfg, artifact_EOG] = ft_artifact_zvalue(cfg);
 
-% Artifact Rejection
+%% Artifact Rejection
 
 cfg=[]; 
-cfg.artfctdef.reject = 'complete'; % this rejects complete trials, use 'partial' if you want to do partial artifact rejection
+cfg.artfctdef.reject = 'partial'; % this rejects complete trials, use 'partial' if you want to do partial artifact rejection
 cfg.artfctdef.eog.artifact = artifact_EOG; % 
-cfg.artfctdef.jump.artifact = artifact_jump;
+% cfg.artfctdef.jump.artifact = artifact_jump;
 cfg.artfctdef.muscle.artifact = artifact_muscle;
-data_no_artifacts = ft_rejectartifact(cfg,data);
+data_no_artifacts = ft_rejectartifact(cfg,data_eeg);
 
+%% Merge "trials" together: DOES NOT WORK
+% data.merge = ft_appenddata(cfg, data_no_artifacts)
 
+%% Define Trial 
 
+cfg = [];
+cfg.dataset = data_no_artifacts;
+cfg.trialfun = 'ft_trialfun_general'; % this is the default
+cfg.trialdef.eventtype  = 'trigger'; %specify event type
+cfg.trialdef.eventvalue  = 22; %specify trigger value;
+cfg.trialdef.poststim=120
+cfg.trialdef.prestim=0
+%cfg.trl = cfg.trialdef.triallength;
+cfg = ft_definetrial(cfg);
+trl=cfg.trl
 
-
+cfg.trl = trl; 			% saved somewhere previously
+trialdata = ft_preprocessing(cfg);	% call preprocessing, putting the output in ‘trialdata’
 
 
   
 %% Frequency analysis
 
 cfg              = [];
+cfg.trials       = 1:369
 cfg.output       = 'pow'; 
 cfg.channel      = 'all';
 cfg.method       = 'mtmconvol';
 cfg.taper        = 'hanning';
-cfg.toi          = [0 : 5 : 1000];
-cfg.foi          = 0:1:25;
+cfg.toi          = [0 : 5 : 120];
+cfg.foi          = 0:1:20;
 cfg.t_ftimwin    = ones(size(cfg.foi)) * 0.5;
-TFRhann = ft_freqanalysis(cfg, data_eeg);
+TFRhann = ft_freqanalysis(cfg, data_no_artifacts);
 
 
 % Plot single channel
@@ -192,8 +233,8 @@ ft_singleplotTFR(cfg, TFRhann);
 cfg = [];
 % cfg.baseline     = [-0.5 -0.1]; 
 % cfg.baselinetype = 'absolute'; 
-cfg.xlim         = [287 412];   
-cfg.zlim         = [0 25];	
+cfg.xlim         = [1 360];   
+cfg.zlim         = [0 10];	
 cfg.ylim         = [7.5 12.5];
 cfg.marker       = 'on';
 cfg.showlabels   = 'yes';	
--- data analysis/preprocess.m
+++ data analysis/preprocess.m
@@ -2,24 +2,40 @@ function preprocess
 
 % Function to preprocess EEG Neuroscan data for spectral analysis
 
-% Load data into EEGLAB
+%%  Load data into EEGLAB
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 EEG = pop_loadcnt('E:\Google Drive\Project AE_SNR EEG ERP\Data\1016\1016_AAT_09_30_2016.cnt' , 'dataformat', 'auto', 'memmapfile', '');
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'gui','off'); 
 EEG = eeg_checkset( EEG );
 
-% Resample to 250 hz
+%% Resample to 250 hz
 EEG = pop_resample( EEG, 250);
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'gui','off'); 
 pop_saveh( ALLCOM, 'eeglabhist.m', 'C:\Users\cwbishop\Documents\MATLAB\');
 EEG = eeg_checkset( EEG );
 
-% Define epochs according to trigger codes
-EEG = pop_selectevent( EEG, 'type',20,'deleteevents','on');
+%% Load channel locations/turn off channel FCZ (ground)/set reference to Cz
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'gui','off'); 
+EEG=pop_chanedit(EEG, 'load',{'C:\\Users\\cwbishop\\Documents\\SNR-ERP\\data analysis\\Neuroscan65.elp' 'filetype' 'autodetect'},'changefield',{64 'datachan' 0});
+[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+EEG = eeg_checkset( EEG );
+EEG = pop_reref( EEG, 64);
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'gui','off'); 
+eeglab redraw;
+
+%% Run ICA
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'gui','off'); 
+pop_saveh( ALLCOM, 'eeglabhist.m', 'C:\Users\cwbishop\Documents\SNR-ERP\data analysis\');
+EEG = eeg_checkset( EEG );
+EEG = pop_runica(EEG, 'extended',1,'interupt','on');
+[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+EEG = eeg_checkset( EEG );
+pop_topoplot(EEG,0, [1:63] ,'CNT file resampled',[8 8] ,0,'electrodes','on');
+eeglab redraw;
+
+%% Define epochs according to trigger codes
+EEG = pop_epoch( EEG, {  '20'  }, [0  120], 'newname', 'CNT file resampled epochs', 'epochinfo', 'yes');
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'gui','off'); 
 EEG = eeg_checkset( EEG );
-pop_eegplot( EEG, 1, 1, 1);
-EEG = eeg_checkset( EEG );
-EEG = pop_rmdat( EEG, {'20'},[0 120] ,0);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'gui','off'); 
 eeglab redraw;
+
