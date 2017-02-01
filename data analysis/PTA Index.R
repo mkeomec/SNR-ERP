1f283a945b13fae2576bda6e34cfac42b92e9af4
smithm59 <smithm59@uw.edu>
2017-01-31 16:25:27 -0800

    Fixed For/if loop

--- data analysis/PTA Index.R
+++ data analysis/PTA Index.R
@@ -1,9 +1,20 @@
-y <- read.csv(file="C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_23-Jan-2017.csv")
+#y <- read.csv(file="C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_23-Jan-2017.csv")
 
-x <- read.csv(file="E:/Google Drive/Project AE_SNR EEG ERP/Documentation/SNR Subject Key.csv")
+y <- read.csv(file="C:/Users/Michael/Documents/GitHub/SNR-ERP/data analysis/alphapower_23-Jan-2017.csv")
+
+#x <- read.csv(file="E:/Google Drive/Project AE_SNR EEG ERP/Documentation/SNR Subject Key.csv")
+
+x <- read.csv(file="C:/Users/Michael/Google Drive/Project AE_SNR EEG ERP/Documentation/SNR Subject Key.csv")
 
 sub_id <- y[[1]]
 
-x["subject_id"=sub_id]
-x[x$Subject_ID %in% sub_id,]
+
+sub_id_PTA <- x[x$Subject_ID %in% sub_id,]
+
+x$Subject_ID %in% sub_id
+
+for(i in 1:length(sub_id)){
+  if (sub_id[i] %in% x$Subject_ID) {
+    y$PTA_Code[y$subject_id==sub_id[i]]<- x[x$Subject_ID == sub_id[i],"PTA"]} 
+  }
 
