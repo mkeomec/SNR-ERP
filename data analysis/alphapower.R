49f8ad8cdce0b48d5238f8b4dc98f9caf9db564e
Michael Lee <selicas@gmail.com>
2017-02-01 17:20:52 -0800

    Sort by alpha power

--- data analysis/alphapower.R
+++ data analysis/alphapower.R
@@ -1,23 +1,32 @@
-alphapower <- read.csv(file= "C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_23-Jan-2017.csv",head=TRUE,sep=",")
- 
-O1_open <- alphapower$O1_AOC_open[-1]
-SNR80 <- alphapower$SNR.80..OLD.[-1]
-PTA <- alphapower$PTA[-1]
-data <- data.frame(O1_open,SNR80,PTA)
+alphapower <- read.csv(file= "C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_Master.csv",head=TRUE,sep=",")
 
-alpha.mod= lm (SNR80~O1_open)
+alphapower <- alphapower[-1,] 
+
+#data <- data.frame(Avg_open,Avg_closed,open_closed.ratio,SNR80,PTA, data=alphapower)
+
+Avg_open <- alphapower$Avg_open
+Avg_closed <- alphapower$Avg_closed
+open_closed.ratio <- alphapower$Open_closed.ratio
+SNR80 <- alphapower$SNR_80_OLD
+PTA <- alphapower$PTA
+Age <- alphapower$Age
+
+alpha.mod= lm (SNR80~Avg_open)
 summary(alpha.mod)
 
-alpha.mod1= lm (alphapower$SNR.80..OLD.~alphapower$PTA)
+alpha.mod1= lm (SNR80~PTA)
 summary(alpha.mod1)
 
-alpha.mod2= lm (alphapower$SNR.80..OLD.~alphapower$PTA+alphapower$O1_AOC_open+alphapower$Age)
+alpha.mod2= lm (SNR80~PTA+Avg_open+Age)
 summary(alpha.mod2)
 
 
 
 
 
-ggplot(data, aes(x=O1_open,y=SNR80))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
+ggplot(alphapower, aes(x=Avg_open,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
 
-ggplot(data, aes(x=PTA,y=SNR80))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
+ggplot(alphapower, aes(x=PTA,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
+
+alpha_sorted=alphapower[order(alphapower$Avg_open),]
+alpha_sorted[c("Row","Avg_open")]
