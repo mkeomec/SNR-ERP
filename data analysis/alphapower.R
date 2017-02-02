4c411a2416d3026b0faa4b140245991b0cdefddb
smithm59 <smithm59@uw.edu>
2017-02-02 15:15:44 -0800

    updated plotting code to remove background grid. additional code format changes

--- data analysis/alphapower.R
+++ data analysis/alphapower.R
@@ -1,9 +1,11 @@
 
 library(ggplot2)
 
-alphapower <- read.csv(file= "C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_Master.csv",head=TRUE,sep=",")
+#alphapower <- read.csv(file= "C:/Users/cwbishop/Documents/SNR-ERP/data analysis/alphapower_Master.csv",head=TRUE,sep=",")
 
-alphapower <- alphapower[-1,] 
+alphapower <- read.csv(file= "C:/Users/Michael/Documents/GitHub/SNR-ERP/data analysis/alphapower_Master.csv",head=TRUE,sep=",")
+
+alphapower <- alphapower[-1,] #removes 1015 subejct info 
 
 #data <- data.frame(Avg_open,Avg_closed,open_closed.ratio,SNR80,PTA, data=alphapower)
 
@@ -23,20 +25,69 @@ summary(alpha.mod1)
 alpha.mod2= lm (SNR80~PTA+Avg_open+Age)
 summary(alpha.mod2)
 
-alpha.mod= lm (SNR_50_OLD~Avg_open, data=alphapower)
-summary(alpha.mod)
+alpha.mod3= lm (SNR_50_OLD~Avg_open, data=alphapower)
+summary(alpha.mod3)
 
-alpha.mod= lm (SNR_80_OLD~open_closed.ratio, data=alphapower)
-summary(alpha.mod)
+alpha.mod4= lm (SNR_80_OLD~open_closed.ratio, data=alphapower)
+summary(alpha.mod4)
 
-ggplot(alphapower, aes(x=Avg_open,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
-ggplot(alphapower, aes(x=Avg_closed,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
-ggplot(alphapower, aes(x=Avg_open,y=SNR_50_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
-ggplot(alphapower, aes(x=Avg_closed,y=SNR_50_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
-ggplot(alphapower, aes(x=open_closed.ratio,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
-ggplot(alphapower, aes(x=Avg_open,y=Avg_closed))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
+ggplot(alphapower, aes(x=Avg_open,y=SNR_80_OLD))+
+  geom_point(size=2, color="black") +
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+  
 
-ggplot(alphapower, aes(x=PTA,y=SNR_80_OLD))+ geom_point(size=5, color="red") +geom_smooth(method="lm", size = 2, se=FALSE)
+ggplot(alphapower, aes(x=Avg_closed,y=SNR_80_OLD))+ 
+  geom_point(size=2, color="black") +
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+
+ggplot(alphapower, aes(x=Avg_open,y=SNR_50_OLD))+
+  geom_point(size=2, color="black")+
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+
+ggplot(alphapower, aes(x=Avg_closed,y=SNR_50_OLD))+
+  geom_point(size=2, color="black")+
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+
+ggplot(alphapower, aes(x=open_closed.ratio,y=SNR_80_OLD))+
+  geom_point(size=2, color="black")+
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+
+ggplot(alphapower, aes(x=Avg_open,y=Avg_closed))+
+  geom_point(size=2, color="black")+
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
+
+ggplot(alphapower, aes(x=PTA,y=SNR_80_OLD))+
+  geom_point(size=2, color="black")+
+  geom_smooth(method="lm", size = 1, se=FALSE)+
+  theme(panel.grid.major = element_blank()
+        , panel.grid.minor = element_blank()
+        , panel.background = element_blank()
+        , axis.line = element_line(colour = "black"))
 
 alpha_sorted=alphapower[order(alphapower$Avg_open),]
 alpha_sorted[c("Row","Avg_open")]
+
